cbe99504cc171dd36eba37bb75796fa094b5cb23
Arthur Pimenta <pimentabraga2@gmail.com>
2020-05-13 22:05:08 -0300

    Reciclando html com partials e criando roda para receber dados com body-parser

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+//EJS e arquivos estaticos
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false})); //Decodificar os dados enviados pelo form
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
+
+app.post("/saveQuestion", (req, res) => {
+    var title = req.body.title;
+    var question = req.body.question;
+    res.send("title: "+title+"<br/>question:"+question);
 });
 
 app.listen(8080, (erro) => {
